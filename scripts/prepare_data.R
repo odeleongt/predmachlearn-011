38ac2b4224e4515ca0e691f05f965d780a25292d
odeleongt [nb] <odeleongt@gmail.com>
2015-02-22 16:53:46 -0600

    Add data to report

--- scripts/prepare_data.R
+++ scripts/prepare_data.R
@@ -1,8 +1,3 @@
-# Load used packages
-library(package = ggplot2)
-library(package = dplyr)
-
-
 # Read the training dataset
 training <- read.csv(file = "./data/pml-training.csv", stringsAsFactors = FALSE,
                      row.names = 1, na.strings = c("#DIV/0!", "", "NA"))
@@ -20,29 +15,18 @@ NAs <- sapply(X = training[!names(training) %in% meta],
 mostly_NA <- names(NAs)[NAs > 0.95]
 
 # Remove summaries and summary variables
-training <- training %>%
-  filter(new_window == "no") %>%
-  select(-one_of(mostly_NA), -new_window)
+training <- subset(x = training, subset = new_window == "no",
+                   select = names(training)[!names(training) %in%
+                                              c(mostly_NA, "new_window")])
 
 # Storage modes to serach for non-numeric variables
 modes <- sapply(X = training, FUN = mode)
 
 # Zero-variance variables
 vars <- sapply(X = training[, modes == "numeric"], FUN = var, na.rm = TRUE)
-names(vars)[vars == 0]
-
-# Search non-numeric values
-table(grep(pattern = "[^-0-9.]",
-           x = unlist(training[, modes != "numeric" &
-                                  !names(training) %in% meta]),
-           value = TRUE))
 
 # Exclude metadata variables from the predictors
 predictors <- names(training)[!names(training) %in% meta]
 
 # Clean dataset including only class and predictors
 training_clean <- training[, c("classe", predictors)]
-
-# Example of non-linear relationship between features
-qplot(data = training_clean, x = roll_forearm, y = pitch_forearm,
-      color = classe, size = I(0.5)) + theme_classic()
